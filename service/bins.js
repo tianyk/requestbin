d95136d56cc4ee19f90bf07a532075d1ccd805e7
tyk <yongketian@gmail.com>
2018-07-05 11:36:09 +0800

    修改bins meta后缀命名

--- service/bins.js
+++ service/bins.js
@@ -2,8 +2,8 @@ const randomstring = require('randomstring');
 const _ = require('lodash');
 const is = require('is-type-of');
 const DB = require('../models').DB;
-const META_SUFFIX = ':meta';
-const BINS_SUFFIX = ':bins';
+const META_NS = ':meta';
+const BINS_NS = ':bins';
 
 // n4N9COuJ:meta {binid, createdAt, private, uid}
 // n4N9COuJ:bins [{binid, method, pathname, header, body, ip..}]
@@ -14,7 +14,7 @@ function isNotFoundError(err) {
 
 async function exist(binid) {
     try {
-        await DB.get(`${binid}${META_SUFFIX}`);
+        await DB.get(`${binid}${META_NS}`);
         return true;
     } catch (err) {
         if (!isNotFoundError(err)) throw err;
@@ -45,13 +45,13 @@ async function meta({ binid, private = false, uid, createdAt = _.now() }) {
         uid,
         createdAt
     };
-    await DB.put(`${binid}${META_SUFFIX}`, JSON.stringify(bin));
+    await DB.put(`${binid}${META_NS}`, JSON.stringify(bin));
     return bin;
 }
 
 async function getMeta(binid) {
     try {
-        let meta = await DB.get(`${binid}${META_SUFFIX}`);
+        let meta = await DB.get(`${binid}${META_NS}`);
         return JSON.parse(meta);
     } catch (err) {
         if (!isNotFoundError(err)) throw err;
@@ -62,7 +62,7 @@ async function getBins(binid) {
     await check(binid);
 
     try {
-        let bins = await DB.get(`${binid}${BINS_SUFFIX}`);
+        let bins = await DB.get(`${binid}${BINS_NS}`);
         return JSON.parse(bins);
     } catch (err) {
         // ignore NotFoundError
@@ -93,7 +93,7 @@ async function bin({ binid, method = 'GET', pathname, version = '1.1', headers, 
         bins = [bin];
     }
 
-    await DB.put(`${binid}${BINS_SUFFIX}`, JSON.stringify(bins));
+    await DB.put(`${binid}${BINS_NS}`, JSON.stringify(bins));
     return bins;
 }
 
@@ -101,8 +101,8 @@ async function del(binid) {
     if (!binid) throw new Error('[del] binid is required.');
 
     await Promise.all([
-        DB.del(`${binid}${META_SUFFIX}`),
-        DB.del(`${binid}${BINS_SUFFIX}`)
+        DB.del(`${binid}${META_NS}`),
+        DB.del(`${binid}${BINS_NS}`)
     ]);
 }
 
